3465d5d5b440bf576663adef9c3f6db37792d88e
Nick Frichette <frichetten@gmail.com>
2016-07-30 17:13:51 -0500

    Third Commit: Ready to start developing

--- app/src/main/java/io/github/frichetten/wifireader/MainActivity.java
+++ app/src/main/java/io/github/frichetten/wifireader/MainActivity.java
@@ -40,10 +40,6 @@ public class MainActivity extends AppCompatActivity {
             int size = wifiScanList.size();
             Log.d("Result", String.valueOf(size));
         }
-
-        if(true){
-
-        }
     }
 
     class WifiScanReciever extends BroadcastReceiver{
