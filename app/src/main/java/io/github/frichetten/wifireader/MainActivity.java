53d8e1dc1d02a547f332c8500703e529249a1300
Nick Frichette <frichetten@gmail.com>
2016-07-31 00:13:33 -0500

    Figured out that hte standard Android API wont let me access individual packets.

--- app/src/main/java/io/github/frichetten/wifireader/MainActivity.java
+++ app/src/main/java/io/github/frichetten/wifireader/MainActivity.java
@@ -1,6 +1,9 @@
 package io.github.frichetten.wifireader;
 
 import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.jar.Manifest;
@@ -25,10 +29,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button findNetworks = (Button) findViewById(R.id.findNetworkButton);
+        findNetworks.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                findNetworks();
+                Intent i = new Intent(getApplicationContext(), findNetworks.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void findNetworks(){
         //Creating the wifi managing objects
         WifiManager wim = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        WifiScanReciever wifiReciever = new WifiScanReciever();
-        registerReceiver(wifiReciever, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        WifiScanReceiver wifiReceiver = new WifiScanReceiver();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 
         //Checking permissions. If there is none, get them. If there is, perform action
         if (checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -38,11 +54,12 @@ public class MainActivity extends AppCompatActivity {
             List<ScanResult> wifiScanList = wim.getScanResults();
             String data = wifiScanList.get(0).toString();
             int size = wifiScanList.size();
-            Log.d("Result", String.valueOf(size));
+            for(int i=0;i<size;i++) {
+                Log.d("Result", wifiScanList.get(i).toString());
+            }
         }
     }
-
-    class WifiScanReciever extends BroadcastReceiver{
+    class WifiScanReceiver extends BroadcastReceiver{
         public void onReceive(Context c, Intent intent){
         }
     }
